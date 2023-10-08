c0143cc4d3f5dc43da1f48ca4aa39f1dee24fc9e
Luciano Nascimento <luciano.m.nasc@gmail.com>
2023-10-08 01:33:15 -0300

    project complete

---
+++ src/main/java/com/allinformatica/parkingcontrol/dtos/ParkingSpotDto.java
@@ -0,0 +1,16 @@
+package com.allinformatica.parkingcontrol.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ParkingSpotDto(@NotBlank String parkingSpotNumber,
+                             @NotBlank @Size(max = 7) String licensePlateCar,
+                             @NotBlank String brandCar,
+                             @NotBlank String modelCar,
+                             @NotBlank String colorCar,
+                             @NotBlank String responsibleName,
+                             @NotBlank String apartment,
+                             @NotBlank String block) {
+
+
+}
---
+++ src/main/java/com/allinformatica/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,54 @@
+package com.allinformatica.parkingcontrol.services;
+
+import com.allinformatica.parkingcontrol.models.ParkingSpotModel;
+import com.allinformatica.parkingcontrol.repository.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ParkingSpotService {
+    final ParkingSpotRepository parkingSpotRepository;
+
+    /* ao invés do '@AutoWired' para injeção de dependências,  utilizou-se aqui
+    o método convencional do contrutor com passagem de parâmetros*/
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository){
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar){
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+
+    public Page<ParkingSpotModel> findAll(Pageable pageable) {
+        return parkingSpotRepository.findAll(pageable);
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        parkingSpotRepository.delete(parkingSpotModel);
+    }
+}
